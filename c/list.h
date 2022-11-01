3de71f63ef46cb8800f02e3c4f5580b7ce3439a7
Guichi Zhao <zgc910619@gmail.com>
2022-11-01 16:26:43 +0800

    list

---
+++ c/list.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Link
+{
+private:
+  Link *_next;
+  int _id;
+
+public:
+  Link(Link *next, int id);
+  ~Link();
+  Link *GetNext() const
+  {
+    return _next;
+  }
+};
+
+Link::Link(Link *next, int id) : _next(next), _id(id)
+{
+  cout << "Create link " << _id << endl;
+}
+
+Link::~Link()
+{
+  cout << "Destory link " << _id << endl;
+}
+
+class List
+{
+private:
+  Link *_head;
+
+public:
+  List() : _head(0)
+  {
+    cout << "Create list " << endl;
+  }
+  ~List()
+  {
+    cout << "Destory list " << endl;
+    while (_head != 0)
+    {
+      Link *head = _head;
+      _head = _head->GetNext();
+      delete head;
+    }
+  }
+  void Add(int id)
+  {
+    Link *head = new Link(_head, id);
+    _head = head;
+  }
+};
--- c/main.cpp
+++ c/main.cpp
@@ -1,29 +1,14 @@
 #include <iostream>
-#include "stack.h"
+#include "list.h"
+// #include "stack.h"
 // #include "calculator.h"
 // #include "input.h"
 using namespace std;
 
-int StrLen(char const pStr[])
-{
-  char const *p = pStr;
-  while (*p++)
-    ;
-  return p - pStr - 1;
-}
-
 int main()
 {
-
-  Stack stack;
-  for (int i = 0; i < 5; ++i)
-  {
-    cout << "Push " << i << endl;
-    stack.Push(i);
-  }
-  Sequencer seq(stack);
-  seq.Log();
-  // Stack *aStack = new Stack[4];
-  // delete [] aStack;
-  // Stack s;
+  List *l = new List();
+  l->Add(1);
+  l->Add(2);
+  delete l;
 }
