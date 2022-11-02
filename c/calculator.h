d34a7b5f9e0f80bd5de54432dd374eb0e36ccb08
Guichi Zhao <zgc910619@gmail.com>
2022-11-02 17:38:14 +0800

    add calculator

--- c/calculator.h
+++ c/calculator.h
@@ -3,55 +3,83 @@
 #include "input.h"
 #include "stack.h"
 using namespace std;
-class Calculator
+class Node
 {
-private:
-  Stack _stack;
-  int Calculate(int n1, int n2, int token)
-  {
-    cout << n1 << ":" << n2 << ":" << token << endl;
-    if (token == '+')
-    {
-      return n1 + n2;
-    }
-  }
-
 public:
-  Calculator();
-  ~Calculator();
-  bool Execute(Input const &input)
+  double virtual Calc() = 0;
+  Node()
   {
-    int token = input.Token();
-    if (token == tokNumber)
-    {
-      _stack.Push(input.Number());
-      return true;
-    }
-    else if (token == tokError)
-    {
-      return false;
-    }
-    else
-    {
-      int n1 = _stack.Pop();
-      int n2 = _stack.Pop();
-
-      _stack.Push(Calculate(n1, n2, token));
-      return true;
-    }
+    cout << "Create Node" << endl;
   }
-  Stack const &GetStack() const
+  virtual ~Node()
   {
-    return _stack;
+    cout << "Destory Node" << endl;
   }
 };
 
-Calculator::Calculator()
+class NumNode : public Node
 {
-  cout << "Calculator created" << endl;
-}
+public:
+  NumNode(double d) : _num(d) {}
 
-Calculator::~Calculator()
+  double Calc()
+  {
+    return _num;
+  }
+
+private:
+  double _num;
+};
+
+class BinNode : public Node
 {
-  cout << "Calculator destoried" << endl;
-}
+public:
+  BinNode(Node *left, Node *right) : _left(left), _right(right)
+  {
+    cout << "Create BinNode" << endl;
+  }
+  ~BinNode()
+  {
+    delete _left;
+    delete _right;
+    cout << "Destory BinNode " << endl;
+  };
+
+protected:
+  Node *_left;
+  Node *_right;
+};
+
+class AddNode : public BinNode
+{
+public:
+  AddNode(Node *left, Node *right) : BinNode(left, right)
+  {
+    cout << "Create Plus Node" << endl;
+  };
+  ~AddNode()
+  {
+    cout << "Destory Plus Node" << endl;
+  }
+  double Calc()
+  {
+    return _left->Calc() + _right->Calc();
+  }
+};
+
+class MultNode : public BinNode
+{
+public:
+  MultNode(Node *left, Node *right) : BinNode(left, right)
+  {
+    cout << "Create Mul Node" << endl;
+  };
+  ~MultNode()
+  {
+    cout << "Destory Plus Node" << endl;
+  }
+  double Calc()
+  {
+    return _left->Calc() * _right->Calc();
+  }
+};
--- c/main.cpp
+++ c/main.cpp
@@ -2,20 +2,20 @@
 #include "hashtable.h"
 // #include "list.h"
 #include "stack.h"
-// #include "calculator.h"
+#include "calculator.h"
 // #include "input.h"
 using namespace std;
 
 int main()
 {
-  StringTable st;
-  st.ForceAdd("hello");
-  st.ForceAdd("world");
-  st.ForceAdd("guichi");
-
-  // cout << st.GetString(2) << endl;
-  // cout << st.GetString(1) << endl;
-  // cout << st.GetString(0) << endl;
-  cout << st.Find("guichi") << endl;
-  // cout << st.Find("zhao") << endl;
+  Node *pNode1 = new NumNode(20.0);
+  Node *pNode2 = new NumNode(-10.0);
+  Node *pNode3 = new AddNode(pNode1, pNode2);
+  Node *pNode4 = new NumNode(21);
+  Node *pNode5 = new MultNode(pNode3, pNode4);
+  cout << "Calculating the tree\n";
+  // tell the root to calculate itself
+  double x = pNode5->Calc();
+  cout << x << endl;
+  delete pNode5; // and all children
 }
