8a42b9236287798833534d6f959c3a82e7de5b87
Guichi Zhao <zgc910619@gmail.com>
2023-02-07 10:58:45 +0800

    save

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
-import { fetchAndInstantiate } from "./util";
-fetchAndInstantiate("hello.wasm",{}).then(function (instance) {
-  var ho = instance.exports.how_old(2021, 2000);
-  console.log(ho)
-  // var ageEl = document.getElementById("age");
-  // ageEl.innerText = ho;e
+import { fetchAndInstantiate } from "./utils";
+import url from "./memory2.wasm?url";
+
+var memory = new WebAssembly.Memory({ initial: 10, maximum: 100 });
+var importObject = {
+  js: { mem: memory },
+};
+fetchAndInstantiate("url", importObject).then(function (instance) {
+  var fibNum = 20;
+  instance.exports.fibonacci(fibNum);
+  var i32 = new Uint32Array(memory.buffer);
+  var container = document.getElementById("container");
+  for (var i = 0; i < fibNum; i++) {
+    container.innerText += `Fib[${i}]: ${i32[i]}\n`;
+  }
 });
