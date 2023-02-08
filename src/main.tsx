5d8c7e223c48dd4f6aef699cbab2c72358a640e3
Guichi Zhao <zgc910619@gmail.com>
2023-02-08 10:12:42 +0800

    move out

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import { fetchAndInstantiate } from "./utils";
-import url from "./memory2.wasm?url";
+import url from "./add.wasm?url";
 
 var memory = new WebAssembly.Memory({ initial: 10, maximum: 100 });
 var importObject = {
   js: { mem: memory },
 };
-fetchAndInstantiate("url", importObject).then(function (instance) {
-  var fibNum = 20;
-  instance.exports.fibonacci(fibNum);
-  var i32 = new Uint32Array(memory.buffer);
-  var container = document.getElementById("container");
-  for (var i = 0; i < fibNum; i++) {
-    container.innerText += `Fib[${i}]: ${i32[i]}\n`;
-  }
+fetchAndInstantiate(url, importObject).then(function (instance) {
+  const x = instance.exports.howOld(100, 10);
+  console.log(x);
 });
