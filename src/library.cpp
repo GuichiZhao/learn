d4b54b960411121db7c22c7a052c888e2a45e85b
Guichi Zhao <zgc910619@gmail.com>
2023-02-15 09:50:01 +0800

    save

--- src/library.cpp
+++ src/library.cpp
@@ -6,9 +6,25 @@ WASM_EXPORT int add(int a, int b)
 }
 WASM_EXPORT void print(const char *str)
 {
+  js_print_string("Print from c++");
   js_print_string(str);
 }
 WASM_EXPORT void helloWorld()
 {
   js_print_string("Hello, World!\n");
+}
+
+// WASM_EXPORT void add1(int size)
+// {
+//    new char[size];
+// }
+
+// WASM_EXPORT char* get_memory_for_char_array(int size)
+// {
+//    return new char[size];
+// }
+
+WASM_EXPORT int *get_memory_for_int_array(int size)
+{
+  return new int[size];
 }
--- src/main.tsx
+++ src/main.tsx
@@ -10,28 +10,28 @@ function charPtrToString(str: number) {
   const memory = get_memory();
   let length = 0;
   for (; memory[str + length] !== 0; ++length) {}
-  console.log(str)
+  console.log(str);
   console.log(memory);
   console.log(length);
   return decoder.decode(memory.subarray(str, str + length));
 }
 const decoder = new TextDecoder("utf-8");
 // const encoder = new TextEncoder("utf-8");
-let printString = function (str) {
+let printString = function (str: string) {
   console.log("str", str);
 };
 const importObject = {
   env: {
-    js_print_string: function (str) {
-      printString(charPtrToString(str));
+    js_print_string: function (ptr: number) {
+      printString(charPtrToString(ptr));
     },
   },
 };
 fetchAndInstantiate(url, importObject).then(function (instance) {
   wasm = instance;
   console.log(instance.exports);
-  // instance.exports.print("aaa");
-  instance.exports.helloWorld();
+  instance.exports.print("aaa");
+  // instance.exports.helloWorld();
   // const result = instance.exports.add(7, 8);
   // console.log(result);
 
