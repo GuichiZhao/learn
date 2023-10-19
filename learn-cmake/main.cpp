dc349673bf39f8b7e81321e2f20d9e2105a4b27a
Guichi Zhao <zgc910619@gmail.com>
2023-10-19 16:00:20 +0800

    submodule

--- learn-cmake/main.cpp
+++ learn-cmake/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-#include "./add/adder.h"
+#include "adder.h"
+#include "GLFW/glfw3.h"
 using namespace std;
-int main(int argc)
+int main()
 {
   cout << "20+30= " << guich::add(20, 30) << endl;
+  GLFWwindow *window;
+  int width, height;
+  if (!glfwInit())
+  {
+    fprintf(stderr, "Failed to initialize GLFW\n");
+    exit(EXIT_FAILURE);
+  }
+  glfwWindowHint(GLFW_DEPTH_BITS, 16);
+  glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+  window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+  // Main loop
+  while (!glfwWindowShouldClose(window))
+  {
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // Terminate GLFW
+  glfwTerminate();
 }
