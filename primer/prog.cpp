5cb5d796150e743bb53ac1dcdbce45e243b231c2
Guichi Zhao <zgc910619@gmail.com>
2023-11-20 13:08:43 +0800

    vite

--- primer/prog.cpp
+++ primer/prog.cpp
@@ -6,19 +6,41 @@
 #include <deque>
 #include <array>
 #include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <functional>
 
 #include "Sales_Data.h"
 #include "header.h"
 
+using namespace std;
+
+class T
+{
+
+  /* data */
+public:
+  string s;
+  T(string s) : s(s)
+  {
+    cout << "constructor: " + s << endl;
+  };
+
+// private:
+  T(const T &t) : s(t.s + " copied")
+  {
+
+    cout << "Copy constrcutor" << endl;
+  }
+};
+void add(T t)
+{
+  cout << t.s << endl;
+}
+
 int main()
 {
-  list<const char *> from{"aa", "bb"};
-  vector<string> to;
-
-  to.assign(from.begin(), from.end());
-
-  for (auto s : to)
-  {
-    cout << s << endl;
-  }
+  T t = string("aa");
+  // T tt = T(string("aa"));
+  // add(string("aa"));
 }
